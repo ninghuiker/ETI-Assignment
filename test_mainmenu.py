912bd0b0ed8392436ee8e719f248212225f2c333
FangTingKoh <41284197+FangTingKoh@users.noreply.github.com>
2020-02-04 00:04:20 +0800

    Pytest for displaying the main menu
    Is not fully working due to the operating system but will re-figure out again/

---
+++ test_mainmenu.py
@@ -0,0 +1,18 @@
+import pytest
+
+from main_menu import *
+
+def test_mainmenu():
+    assert ("""Main Menu
+=========
+[1] Read and load maze from file
+[2] View Maze
+[3] Play maze game
+[4] Configure current maze
+
+[0] Exit Maze\n""")
+
+    
+    
+    
+
